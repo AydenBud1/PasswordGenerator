12d7c9fd451692016e2b37977f768ad581a5535c
Ayden Budish <ab44990@ALA.local>
2022-11-16 09:02:22 -0700

    firstcode

---
+++ javascript.js
@@ -0,0 +1,2 @@
+var firstname=["donovan","Joshua","winston","kenyon","koloy","jonathan","weston","louis","warren","aiden","ayden","alex","henery","kayden","tyler","mason","nathan","winston","aaron","tristan","phiphatphan"]
+var lastname=["fraser","suwnnkit","kelly","budish","ludwig","post","arambula","griffen","palka","teuimo","kerkey","alernandez","lee","callano","daffin","pease","stanley","perfecto","santillana","miranda"]
